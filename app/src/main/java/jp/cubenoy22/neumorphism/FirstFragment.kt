44d984b5e36c64725c25949e460344f48001b015
cubenoy22 <cubenoy22@gmail.com>
2020-12-09 22:40:23 +0900

    Add default project files

---
+++ app/src/main/java/jp/cubenoy22/neumorphism/FirstFragment.kt
@@ -0,0 +1,11 @@
+package jp.cubenoy22.neumorphism
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment(R.layout.fragment_first)
